b324813303972d71d1ef2e7c416c1b798da9ef02
ninolis <97913013+ninolis@users.noreply.github.com>
2022-11-19 14:49:57 +0200

    create js-66-03 lesson code

---
+++ js/js-66-03.js
@@ -0,0 +1,143 @@
+'use strict';
+// let a = 5;
+// let b = a;
+
+// console.log(a, b);
+
+// a = 8;
+// console.log(a, b);
+
+// const arr = [1, 2, 3];
+// const arr2 = arr;
+
+// console.log(arr, arr2);
+// console.log(arr === arr2);
+
+// console.log();
+
+// arr[0] = 55;
+
+// console.log(arr, arr2);
+// console.log(arr === arr2);
+
+// const arr3 = [55, 2, 3];
+
+// console.log(arr, arr3);
+// console.log(arr === arr3);
+
+// const arr4 = Array.from(arr);
+// console.log(arr, arr4);
+// console.log(arr === arr4);
+
+// The slice() method returns a shallow copy of a portion of an array into a new array object selected from start to end (end not included) where start and end represent the index of items in that array. The original array will not be modified.
+
+// Syntax
+
+// slice()
+// slice(start)
+// slice(start, end)
+
+// Return a portion of an existing array
+
+// const fruits = ['Banana', 'Orange', 'Lemon', 'Apple', 'Mango', 'Grapes'];
+// const citrus = fruits.slice(1, 3);
+
+// fruits contains ['Banana', 'Orange', 'Lemon', 'Apple', 'Mango']
+// citrus contains ['Orange','Lemon']
+// const citrus2 = fruits.slice(); // copy whole array
+// console.log(citrus2);
+// const citrus3 = fruits.slice(-1); // copy last element of array
+// console.log(citrus3);
+
+// const citrus4 = fruits.slice(-2); // copy last two elements of array
+// console.log(citrus4);
+
+// const citrus5 = fruits.slice(1, -2); // copy from index 1 till end -2
+// console.log(citrus5);
+/////////////////////////////////////////////////
+/*
+const arr = [1, 2, , 4, , 6];
+// console.log(arr);
+// console.log(arr.indexOf(''));// -1
+// console.log(arr[2]);// undefined
+// console.log(arr.indexOf(undefined));// -1
+// console.log(arr[2]); // undefined
+
+//! = = = = = = = = = = = = = = = = = = =
+find indexes of empty element (which is undefined) in array arr
+const emptyEl = [];
+for (let i = 0; i < arr.length; i += 1) {
+  // console.log(arr[i]);
+  if (typeof arr[i] === 'undefined') {
+    emptyEl.push(i);
+  }
+}
+
+console.log(emptyEl);
+*/
+
+///////////////////////////////////////////////
+// JS V2 block-3 Autochecking  ASSIGNMENT 00/41  Status: Done
+
+/*
+** THEORY
+
+
+** TASK
+
+
+** Test
+
+
+ */
+
+// Javascript Editor:
+// START
+/*
+
+*/
+
+// Javascript Editor:
+// FINISH
+/*
+
+console.log(
+*/
+
+// RESULT
+/*
+
+*/
+
+///////////////////////////////////////////////
+// JS V2 block-3 Autochecking  ASSIGNMENT 00/41  Status: Done
+
+/*
+** THEORY
+
+
+** TASK
+
+
+** Test
+
+
+ */
+
+// Javascript Editor:
+// START
+/*
+
+*/
+
+// Javascript Editor:
+// FINISH
+/*
+
+console.log(
+*/
+
+// RESULT
+/*
+
+*/

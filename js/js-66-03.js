1d1096988000fc1b38e1a7b6bf44c34f7b0774d9
ninolis <97913013+ninolis@users.noreply.github.com>
2022-11-20 09:39:45 +0200

    Update js-66-03.js

--- js/js-66-03.js
+++ js/js-66-03.js
@@ -1,4 +1,5 @@
 'use strict';
+
 // let a = 5;
 // let b = a;
 
@@ -75,6 +76,209 @@ for (let i = 0; i < arr.length; i += 1) {
 
 console.log(emptyEl);
 */
+
+//! = = = = = = = = = = = = = = = = = = =
+/*
+Модуль 2. Заняття 3. Масиви
+Example 1 - Базові операції з масивом
+Створіть масив genres з елементами «Jazz» та «Blues».
+Додайте «Рок-н-рол» до кінця.
+Виведіть у консоль перший елемент масиву.
+Виведіть у консоль останній елемент масиву. Код повинен працювати для масиву довільної довжини.
+Видаліть перший елемент та виведіть його в консоль.
+Вставте «Country» та «Reggae» на початок масиву.
+const genres = ;
+*/
+/*
+const genres = ['Jazz', 'Blues'];
+genres.push('Rock-n-Roll');
+console.log(genres[0]);
+const genresLength = genres.length - 1;
+console.log(genres[genresLength]);
+console.log(genres.shift());
+console.log(genres);
+or using splice
+console.log(genres.splice(0, 1)[0]); // splice returns (from position, number of elements), this new array from which we need first el - index[0]
+genres.unshift('Country', 'Reggae');
+console.log(genres);
+
+OR using splice
+Syntax
+splice(start)
+splice(start, deleteCount)
+splice(start, deleteCount, item1)
+splice(start, deleteCount, item1, item2, itemN) // items to insert item1, item2, itemN
+
+console.log(genres.splice(0, 1, 'Country', 'Reggae')[0]); // splice returns (from position, number of elements), this new array from which we need first el - index[0]
+
+
+*/
+
+/////////////////////////////////////////////////
+/*
+Example 2 - Масиви та рядки
+Напиши скрипт для обчислення площі прямокутника зі сторонами, значення яких зберігаються у змінній values у вигляді рядка. Значення гарантовано розділені пробілом.
+
+const values = '8 11';
+
+*/
+// const values = '8 11';
+// const numbers = values.split(' ');
+// const rectangualarSquare = numbers[0] * numbers[1];
+// console.log(rectangualarSquare);
+
+/////////////////////////////////////////////////
+/*
+Example 3 - Перебір масиву
+Напиши скрипт для перебору масиву fruits циклом for. Для кожного елемента масиву виведи в консоль рядок у форматі номер_елемента: значення_елемента. Нумерація елементів повинна починатися з 1.
+
+const fruits = ['🍎', '🍇', '🍑', '🍌', '🍋'];
+
+*/
+
+// const fruits = ['🍎', '🍇', '🍑', '🍌', '🍋'];
+
+// for (let i = 0; i < fruits.length; i += 1) {
+//   console.log(`${i + 1}: ${fruits[i]}`);
+// }
+
+/////////////////////////////////////////////////
+/*
+Example 4 - Масиви та цикли
+Напиши скрипт, який виводить у консоль ім'я та телефонний номер користувача. У змінних names та phones зберігаються рядки імен та телефонних номерів, розділені комами. Порядковий номер імен та телефонів у рядках вказують на відповідність. Кількість імен та телефонів гарантовано однакова.
+
+const names = 'Jacob,William,Solomon,Artemis';
+const phones = '89001234567,89001112233,890055566377,890055566300';
+
+*/
+
+// const names = 'Jacob,William,Solomon,Artemis';
+// const phones = '89001234567,89001112233,890055566377,890055566300';
+
+// const splittedNames = names.split(',');
+// const splittedPhones = phones.split(',');
+
+// for (let i = 0; i < splittedNames.length; i += 1) {
+//   console.log(`${i + 1}. ${splittedNames[i]}: ${splittedPhones[i]}.`);
+// }
+
+/////////////////////////////////////////////////
+/*
+Example 5 - Масиви та рядки
+Напиши скрипт, який виводить у консоль усі слова рядка крім першого і останнього. Результуючий рядок не повинен починатися або закінчуватися символ пробілу. Скрипт повинен працювати для будь-якого рядка.
+
+const string = 'Welcome to the future';
+
+*/
+
+// const string = 'Welcome to the future nino lis from Kyiv Welcome';
+
+// const words = string.split(' ');
+// console.log(words.slice(1, -1).join(' '));
+
+/////////////////////////////////////////////////
+/*
+Example 6 - Масиви та рядки
+Напиши скрипт, який «розгортає» рядок (зворотний порядок букв) і виводить його в консоль.
+
+const string = 'Welcome to the future';
+
+*/
+
+// const string = 'Welcome to the future nino lis from Kyiv Pryvit';
+
+// const letters = string.split('');
+// const reverseLetters = [];
+// let reverseString;
+
+// // console.log(letters);
+
+// for (let i = letters.length - 1; i >= 0; i -= 1) {
+//   reverseLetters.push(letters[i]);
+// }
+// console.log(reverseLetters.join(''));
+
+/////////////////////////////////////////////////
+/*
+Example 7 - Сортування масиву із циклом
+Напиши скрипт сортування масиву рядків в алфавітному порядку за першою літерою елемента.
+
+const langs = ['python', 'javascript', 'c++', 'haskel', 'php', 'ruby'];
+
+*/
+/*
+const string = 'Welcome to the future nino lis from Kyiv Pryvit';
+const string2 = '!erutuf eht ot emocleW .sil oninl morf viyK tivyrP';
+console.log(string2);
+const letters = string.split('');
+const reverseLetters = [];
+let reverseString;
+
+const letters2 = string2.split('');
+const reverseLetters2 = [];
+let reverseString2;
+console.log(letters2);
+
+console.log(letters);
+
+for (let i = letters.length - 1; i >= 0; i -= 1) {
+  reverseLetters.push(letters[i]);
+}
+console.log(reverseLetters.join(''));
+
+for (let i = letters2.length - 1; i >= 0; i -= 1) {
+  reverseLetters2.push(letters2[i]);
+}
+console.log(reverseLetters2.join(''));
+*/
+
+/////////////////////////////////////////////////
+/*
+Example 8 - Пошук елемента
+Напиши скрипт пошуку найменшого числа у масиві. Код повинен працювати для будь-якого масиву чисел. Використовуй цикл для розв'язання задачі.
+
+const numbers = [2, 17, 94, 1, 23, 37];
+let min;
+
+console.log(min); // 1
+*/
+// const numbers = [2, 17, 94, 1, 23, 37];
+// let min;
+// min = 100000000;
+
+// for (let j = 0; j < numbers.length - 1; j += 1) {
+//   // console.log(min);
+//   if (numbers[j] < min) {
+//     // console.log(numbers[j]);
+//     min = numbers[j];
+//   }
+//   // console.log(min);
+// }
+// console.log(min); // 1
+
+// Lesson 2-2 18-11-2022 Олексій Сімак
+
+/////////////////////////////////////////////////
+/*
+Що буде виведено в консоль?
+function foo(array) {
+// for (let j = 0; j < array.length; j += 1) {
+//   array[j] *= 2;
+// }
+const numbers = [1, 2, 3, 4, 5];
+foo(numbers);
+console.log(numbers); // 1
+*/
+function foo(array) {
+  for (let j = 0; j < array.length; j += 1) {
+    array[j] *= 2;
+  }
+}
+
+const numbers = [1, 2, 3, 4, 5];
+
+foo(numbers);
+console.log(numbers); // 1
 
 ///////////////////////////////////////////////
 // JS V2 block-3 Autochecking  ASSIGNMENT 00/41  Status: Done
